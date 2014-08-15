0c827cf88d8bf71595edf3973e0baee838b1f3c1
Alberto Navarro <alnavarro@gmail.com>
2014-08-15 18:04:53 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express    = require('express');
+	app        = express();
+	bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+ 
+
+
+var port = process.env.PORT || 8080;
+
+require(__dirname +'/router')(app);
+
+app.listen(port);
+console.log('Server running on port ' + port);
